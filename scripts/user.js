c7d070314a1cd5987f06934316f9eb4672cb07a6
dmehra2102 <deepanshumehra2102@gmail.com>
2022-12-15 23:35:58 +0530

    till sorting and filtering part is done.

--- scripts/user.js
+++ scripts/user.js
@@ -1,3 +1,28 @@
+const genre = document.querySelector(".genre_filter");
+genre.addEventListener("change", async (event)=>{
+	let val = event.target.value;
+	if(val===""){
+		FetchAllData();
+	}else{
+		let data = await fetch(`http://localhost:8080/books?genre=${val}`);
+		let new_data = await data.json();
+		ShowData(new_data);
+	}
+})
+
+
+const cost = document.querySelector(".cost_filter");
+cost.addEventListener("change", async (event)=>{
+	let val = event.target.value;
+	if(val!==""){
+		let data = await fetch(`http://localhost:8080/books?_sort=cost&_order=${val}`);
+		let new_data = await data.json();
+		ShowData(new_data);
+	}else{
+		FetchAllData();
+	}
+})
+
 async function FetchAllData() {
 	try {
 		  let data = await fetch("http://localhost:8080/books");
@@ -24,13 +49,63 @@ function ShowData(datas) {
 		      <p>Publisher :${items.publisher}</p>
 		      <p>Cost :${items.cost}</p>
 		      <p>Borrowed :${items.borrowed}</p>
-			  <button>Borrow</button>
+			  <button onClick="HandleBorrow(['${items.book_name}','${items.image_url}',
+			  '${items.author}','${items.genre}','${items.edition}','${items.borrowed}',
+			   '${items.cost}',${items.id}])">Borrow</button>
 		 </div>
 	  </div>
 	  `;
 	});
 
 	mainContainer.innerHTML = new_data.join(" ");
+}
+var modal = document.getElementById("myModal");
+
+function HandleBorrow(details){
+	modal.style.display = "block";
+	document.querySelector(".modal-content").innerHTML = "";
+	document.querySelector(".modal-content").innerHTML = `
+	<div class="cards">
+		 <div class="image_div"><img src='${details[1]}' /></div>
+		 <div class="book_info">
+		      <h3>${details[0]}</h3>
+		      <p>Author : ${details[2]}</p>
+		      <p>Genre : ${details[3]}</p>
+		      <p>Edition : ${details[4]}</p>
+		      <p>Cost :${details[6]}</p>
+		      <p>Borrowed :${details[5]}</p>
+			  <button onClick="Close()">Cancel</button>
+			  <button onClick="ConfirmBook(${details[7]})">Confirm</button>
+		 </div>
+	  </div>
+	`;
+}
+
+function Close(){
+	modal.style.display = "none";
+}
+
+
+async function ConfirmBook(id){
+	try {
+		let updated_data = await fetch(
+			`http://localhost:8080/books/${id}`,
+			{
+				  method: "PATCH",
+				  headers: {
+						"Content-Type": "application/json",
+				  },
+				  body: JSON.stringify({borrowed : true}),
+			}
+	  );
+
+	  if (updated_data.ok) {
+			modal.style.display = "none";
+			FetchAllData();
+	  }
+	} catch (error) {
+		console.log(error);
+	}
 }
 
 FetchAllData();
